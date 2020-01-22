3619e33421af9673b22bc19c4a300cfe4f454b56
janelle mellor <janellemellor@gmail.com>
2020-01-22 10:41:27 -0800

    add render function

---
+++ products/renderApparel.js
@@ -0,0 +1,28 @@
+export default function renderApparel(apparel) {
+    const li = document.createElement('li');
+    li.className = apparel.category;
+    li.title = apparel.description;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = apparel.name;
+    li.appendChild(h3);
+
+    const p = document.createElement('p');
+    p.className = 'price';
+
+    const USDollar = '$' + apparel.price.toFixed(2);
+    p.textContent(USDollar);
+    
+    const img = document.createElement('img');
+    img.src = '../assets'; + apparel.image;
+    li.appendChild(img);
+
+    const button = document.createElement('button');
+    button.textContent = 'Add';
+    button.value = apparel.id;
+    p.appendChild(button);
+
+    li.appendChild(p);
+
+    return li;
+} 
