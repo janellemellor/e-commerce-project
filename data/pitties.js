712c7834e98b97dc8a89d44e730989d75db6bea4
janelle mellor <janellemellor@gmail.com>
2020-01-22 09:52:48 -0800

    create product list

---
+++ data/pitties.js
@@ -0,0 +1,69 @@
+const sweater = {
+    id: 'sweater', 
+    name: 'Monkey Sweater',
+    image: 'monkey sweater.jpeg',
+    description: 'a cozy sweater with your favorite sock monkey that you know and love',
+    category: 'apparel',
+    price: 26.98,
+};
+
+const hoodie = {
+    id: 'hoodie', 
+    name: 'grey hoodie',
+    image: 'pit bull hoodie.jpeg',
+    description: 'an everyday hoodie to keep you pitty dry and warm',
+    category: 'apparel',
+    price: 15.99,
+};
+
+const jacket = {
+    id: 'jacket', 
+    name: 'green jacket',
+    image: 'pit bull jacket.jpeg',
+    description: 'a delux jacket to keep your pitty toasty in the cold weather',
+    category: 'apparel',
+    price: 35.97,
+};
+
+const raincoat = {
+    id: 'raincoat', 
+    name: 'yellow raincoat',
+    image: 'rainjacket.jpeg',
+    description: 'a classic raincoat that will keep even the most rain-avoidant pitty dry',
+    category: 'apparel',
+    price: 17.99,
+};
+
+const hat = {
+    id: 'hat', 
+    name: 'chef hat',
+    image: 'chef hat.jpeg',
+    description: 'the perfect hat for your little chef',
+    category: 'apparel',
+    price: 12.99,
+};
+
+const tShirt = {
+    id: 'shirt', 
+    name: 'tshirt',
+    image: 'tshirt.jpeg',
+    description: 'the t for your pit who likes to lounge in style',
+    category: 'apparel',
+    price: 15.48,
+};
+
+const glasses = {
+    id: 'glasses', 
+    name: 'reading glasses',
+    image: 'glasses.jpeg',
+    description: 'perfect for every hipster pit',
+    category: 'apparel',
+    price: 12.47,
+};
+
+const apparel = [
+    sweater, hoodie, jacket, raincoat, hat, tShirt, glasses
+];
+    
+export default apparel;
+
