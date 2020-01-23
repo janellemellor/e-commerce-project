db980f75d120809590216f0f7a54e73973536d45
janelle mellor <janellemellor@gmail.com>
2020-01-23 09:16:34 -0800

    add calcLineItem

--- common/utils.js
+++ common/utils.js
@@ -1,11 +1,20 @@
-export default function findById(itemId, apparelArray) {
-    for (let i = 0; i < apparelArray.length; i++){
-        const arrayItem = apparelArray[i];
+export default function findById(itemId, cartArray) {
+    for (let i = 0; i < cartArray.length; i++){
+        const arrayItem = cartArray[i];
         if (itemId === arrayItem.id) {
             return arrayItem;
         }
     }
 }
+
+
+export function calcLineItem(numberOfItems, itemPrice) {
+    const price = numberOfItems * itemPrice;  
+    const priceRounded = Math.round(price * 100) / 100;
+    return priceRounded;
+}
+
+
 
 
 
--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,20 @@
 export default [
     { id: '2', 
         name: 'grey hoodie',
+        price: 15.99,
+
+
     },
 
     {
         id: '4', 
         name: 'yellow raincoat',
+        price: 23.5,
     },
 
     { id: '7', 
         name: 'reading glasses',
+        price: 8.99,
 
     },
 
