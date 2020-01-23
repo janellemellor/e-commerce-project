cd6a800a455756ca60de5ff337b8f2c8dfce6746
janelle mellor <janellemellor@gmail.com>
2020-01-23 09:47:16 -0800

    add test for calcLineItem

--- test/test.utils.js
+++ test/test.utils.js
@@ -1,6 +1,6 @@
 import findById from '../common/utils.js';
 import cartData from '../data/cart.js';
-
+import {calcLineItem} from '../common/utils.js';
 
 const test = QUnit.test;
 
@@ -13,4 +13,16 @@ test('find by id test', function(assert) {
     assert.equal(result, expect);
 });
 
+
+test('calculate line items', function(assert) {
+    const numberOfItems = '2';
+    const itemPrice = 15.99;
+    const expect = 31.98;
+
+    const result = calcLineItem(numberOfItems, itemPrice);
+
+
+
+    assert.equal(result, expect);
+});
 
