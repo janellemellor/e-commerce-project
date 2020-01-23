20a43cd5cd443d7afb773747cc3340589de45875
janelle mellor <janellemellor@gmail.com>
2020-01-22 22:28:48 -0800

    add cart data

---
+++ data/cart.js
@@ -0,0 +1,17 @@
+export default [
+    { id: '2', 
+        name: 'grey hoodie',
+    },
+
+    {
+        id: '4', 
+        name: 'yellow raincoat',
+    },
+
+    { id: '7', 
+        name: 'reading glasses',
+
+    },
+
+];
+
