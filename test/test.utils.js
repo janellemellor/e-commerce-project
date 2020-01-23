98b1531cfdf4c4b2df0df8b8abde926d61fe9a00
janelle mellor <janellemellor@gmail.com>
2020-01-23 08:42:41 -0800

    add test for findById

--- test/test.utils.js
+++ test/test.utils.js
@@ -1,14 +1,14 @@
-import findById from '../common/utils';
-import apparel from '../data/apparel';
+import findById from '../common/utils.js';
+import cartData from '../data/cart.js';
 
 
 const test = QUnit.test;
 
 test('find by id test', function(assert) {
+    const id = '4';
+    const expect = cartData[1];
     
-    const expect = findById('2', 'hoodie');
-    
-    const result = findById('2', apparel);
+    const result = findById(id, cartData);
    
     assert.equal(result, expect);
 });
