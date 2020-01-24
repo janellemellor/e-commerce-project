de2b75b83f488daa03192ac524b5758ab2e8b159
janelle mellor <janellemellor@gmail.com>
2020-01-24 09:10:18 -0800

    add cart generation

--- cart/shopping-cart.js
+++ cart/shopping-cart.js
@@ -2,27 +2,21 @@ import apparel from '../data/apparel.js';
 import cartData from '../data/cart.js';
 import renderLineItems from '../cart/render-line-items.js';
 import findById from '../common/utils.js';
-import { calcLineItem } from '../common/utils.js';
 
-// find corresponding product by id
-//render line items with cartData[i], correspondingproduct
 
 const table = document.getElementById('table');
 
-function addItemsToShoppingCart(cartItems) = {
-    for (let i = 0; i < apparel.length; i ++) 
+for (let i = 0; i < apparel.length; i ++) {
+    let cartItems = cartData[i];
 
-} 
+    const findCartItemsById = findById(apparel.id, cartItems);
 
-        // Step 4: Generate Shopping Cart
-        // In your /shopping-cart/shopping-cart.js, you will need to:
+    const generateCart = renderLineItems(cartData, findCartItemsById);
+
+    table.appendChild(generateCart);
+   
+}
 
 
-        // Import your data (both cart and products), DOM generation function, and any needed utility functions
-        // locate the tbody element where your line items will go
-        // loop through your data
-        // Create a variable based on the current array index.
-        // Use your findById function to find the corresponding product for this line item
-        // Pass these to your DOM generation function and capture result in variable
-        // Append to the table body (tbody)
-        // git add, commit, push!
+
+
